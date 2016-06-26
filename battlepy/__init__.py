4a4e7887aa0a36a536e4024bf5343951d0d7e7be
Jonathan <jwdryden@gmail.com>
2016-06-26 14:51:49 -0400

    initial

--- battlepy/__init__.py
+++ battlepy/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Jonathan Dryden
 # See LICENSE for details
 __author__      = 'Jonathan Dryden'
-__email__       = 'hello@jwd.io'
+__email__       = ''
 __license__     = 'WTFNMFPL'
 __version__     = '0.1.0'
 __description__ = 'A Python wrapper for the BattleNet API'
